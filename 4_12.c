e2f5fa92416178b02c7d56402b10ca6a86da8eae
Natavrik <44323332+Natavrik@users.noreply.github.com>
2018-10-25 01:10:52 +0300

    Create 4_12.c

---
+++ 4_12.c
@@ -0,0 +1,15 @@
+/* вывести на экран таблицу ASCLL симвлов */
+
+#include<stdio.h>
+
+int main()
+{
+  int i;
+  for (i = 32; i<=124; i++)
+    { 
+      printf("%3d  %c ", i, i);
+      if (i%6 == 0) printf("\n");
+    }
+  printf("\n");
+  return 0;
+}
