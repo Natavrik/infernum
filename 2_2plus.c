44fcbe0143c6bfa9102a39716d14084b5a7627ce
Natavrik <44323332+Natavrik@users.noreply.github.com>
2018-10-25 00:22:17 +0300

    Create 2_2plus.c

---
+++ 2_2plus.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+  float t;
+  printf("Введите температуру на улице: ");
+  scanf("%f", &t);
+  if (t < 0)
+    printf("\n Морозно!\n");
+  else
+    if (t>=0 && t<10)
+      printf("\n Прохладно\n");
+    else
+      if (t>=10 && t<20)
+        printf("\n Нормально\n");
+      else
+	printf("\n Жарко\n");
+  return 0;
+}
