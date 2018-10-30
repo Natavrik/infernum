17f18e07c3fb7ee53098e85b5d9671d7585664a7
Natavrik <44323332+Natavrik@users.noreply.github.com>
2018-10-30 14:02:59 +0300

    Create w2.c

---
+++ qwer/w2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a; 
+    char s;
+     s = getchar ();
+    scanf("%d", &a);
+   
+    printf("Число: %d/n Символ: %c /n ", a , s);
+   
+
+    return 0;
+}
