9ab2e69b5fe79df886ad9b643aae287450c42759
Natavrik <44323332+Natavrik@users.noreply.github.com>
2018-11-06 15:54:32 +0300

    Add files via upload

---
+++ l5_8.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int Dise_Two(void)
+{
+    int q,w;
+    srand(time(NULL));
+    q=rand()%6+1;
+    w=rand()%6+1;
+    q+=w;
+    return q;
+}
+
+int main()
+{
+      int Money;
+    while();
+
+    int Money;
+    srand(time(NULL));
+    MB=rand()%1000+1;
+    printf("Денег в банке: %d\n\n",MB);
+
+    printf("Начнем игру? Делайте ваши ставки!\n");
+    scanf("%d",Money);
+    if(Money == 0)
+
+
+
+    printf("Начнем иру? (1-Да. 2-Нет)\n\n");
+
+    return 0;
+}
