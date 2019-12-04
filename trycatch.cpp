7b6d5b8adb4e0bfedd4317fc80d66f1ce72d285b
Natavrik <44323332+Natavrik@users.noreply.github.com>
2019-12-04 08:38:08 +0000

    Add files via upload

---
+++ trycatch.cpp
@@ -0,0 +1,34 @@
+﻿/*
+
+
+
+
+			| ============================ | trycatch | ============================ | 
+
+
+
+
+
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(0, "");
+	try
+	{
+		int i;
+		cin >> i;
+		if (i == 0)	throw "деление на ноль";
+		//i = 10 / i;
+		if (i == 10) throw 100;
+		if (i == 2) throw 100.0;
+		int* j = new int[i];
+	}
+	catch (const int) { cout << "***\n\n"; }
+	catch (const double) { cout << "_______ \n\n"; }
+	catch (const char* s) { cout << s << endl; }
+	catch(...) { cout << "упс, ошибка!\n\n"; }
+	return 0;
+}
+
